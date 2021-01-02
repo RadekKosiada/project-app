d9102b8114da86429fd98000a30fd1154027f277
RadekKosiada <kosiadar@gmail.com>
2021-01-02 20:57:54 +0100

    Render questions dynamically

--- components/main-screens/home.js
+++ components/main-screens/home.js
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, CommonActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
@@ -15,9 +15,8 @@ import {
 
 import QuestionButton from "../questions-button";
 
-const questionsArray = require('./questions.json');
-
-console.log(questionsArray);
+const questionsData = require("./questions.json");
+const questionsArray = questionsData.questions;
 
 const HomeStack = createStackNavigator();
 
@@ -31,22 +30,37 @@ function HomeStackScreen() {
   );
 }
 
-const Home = ({ navigation }) => {  
+const Home = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button
-        color = "gray"
-        title="Question 1"
-        onPress={() => navigation.navigate("Question 1")}
-      />
+      {questionsArray.map((item, index) => {
+        let question = item.question;
+        return (
+        <Button 
+          key = {index}
+          color="gray" 
+          title={question} 
+          onPress={() => navigation.dispatch(
+            CommonActions.navigate({
+              name: question,
+              params: {
+                key: index
+              }
+            })
+          )}
 
-      <Button
-        color = "gray"
-        title="Question 2"
-        onPress={() => navigation.navigate("Question 2")}
-      />
+          />)
+        })
+      }
     </View>
   );
 };
+
+// TODO The action 'NAVIGATE' with payload {"name":"How did you sleep last night?","params":{"key":0}} was not handled by any navigator.
+// Do you have a screen named 'How did you sleep last night?'?
+/* 
+create component that will be dynamically created as the questions
+*/
+
 
 export default HomeStackScreen;
