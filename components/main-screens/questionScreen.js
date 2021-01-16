c91d8915b352393b9a24406e0e101390954f51fc
RadekKosiada <kosiadar@gmail.com>
2021-01-16 18:22:40 +0100

    Add goToButton as 'close' to QuestionScreen

--- components/main-screens/questionScreen.js
+++ components/main-screens/questionScreen.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native"; 
+import { Text, View } from "react-native";
 
 // // Components
 import SliderComponent from "./sliderComponent";
 import OpenAnswerComponent from "./openAnswerComponent";
 import TagsComponent from "./tagsComponent";
+
+// Button
+import GoToButton from "./goToButton";
 
 //Screen
 function QuestionScreen({ route, navigation }) {
@@ -12,26 +15,35 @@ function QuestionScreen({ route, navigation }) {
   const singleAnswerArray = route.params.singleAnswerArray;
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>
-        {route.params.key + 1} / {route.params.allQuestionsSum}{" "}
-      </Text>
-      <Text>{route.params.question}</Text>
-      {/* <Text>{route.params.possibleAnswer}</Text> */}
-      {route.params.possibleAnswer === "scale" ? <SliderComponent /> : null}
-      {route.params.possibleAnswer === "tags" ? (
-        <TagsComponent 
-          answers={tagsArray} 
-          indefiniteAnswers={true} 
-          question = {route.params.question} 
-          questionNumber = {route.params.key + 1} 
-          allQuestionsSum = {route.params.allQuestionsSum}
-        />
-      ) : null}
-      {route.params.possibleAnswer === "open" ? <OpenAnswerComponent /> : null}
-      {route.params.possibleAnswer === "single choice" ? (
-        <TagsComponent answers={singleAnswerArray} indefiniteAnswers={false} />
-      ) : null}
+    // main View
+    <View>
+        <GoToButton style={{ width: 10, alignItems: "right", justifyContent: "left" }} screenName="Home" data={{ iconName: "close" }} />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          {route.params.key + 1} / {route.params.allQuestionsSum}{" "}
+        </Text>
+        <Text>{route.params.question}</Text>
+        {/* <Text>{route.params.possibleAnswer}</Text> */}
+        {route.params.possibleAnswer === "scale" ? <SliderComponent /> : null}
+        {route.params.possibleAnswer === "tags" ? (
+          <TagsComponent
+            answers={tagsArray}
+            indefiniteAnswers={true}
+            question={route.params.question}
+            questionNumber={route.params.key + 1}
+            allQuestionsSum={route.params.allQuestionsSum}
+          />
+        ) : null}
+        {route.params.possibleAnswer === "open" ? (
+          <OpenAnswerComponent />
+        ) : null}
+        {route.params.possibleAnswer === "single choice" ? (
+          <TagsComponent
+            answers={singleAnswerArray}
+            indefiniteAnswers={false}
+          />
+        ) : null}
+      </View>
     </View>
   );
 }
