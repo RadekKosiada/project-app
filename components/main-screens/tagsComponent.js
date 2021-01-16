5dc5f2d71c4a81a4b007f67a3b97aac2365ded15
RadekKosiada <kosiadar@gmail.com>
2021-01-16 19:20:20 +0100

    Adjust FLEX for TAGS

--- components/main-screens/tagsComponent.js
+++ components/main-screens/tagsComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View, Button, ButtonView, Dimensions } from "react-native";
 import GoToButton from "./goToButton";
+import { FlatList } from "react-native-gesture-handler";
 
 function TagsComponent(props) {
   const answersArray = props.answers;
@@ -8,37 +9,50 @@ function TagsComponent(props) {
   const question = props.question;
   const questionNumber = props.questionNumber;
   const allQuestionsSum = props.allQuestionsSum;
+  const { width } = Dimensions.get('window');
 
+  // <Grid>
+  // Flex https://www.newline.co/30-days-of-react-native/day-05-layout-with-flexbox
 
   console.log(props);
   return (
-    <View style={{ flex: 1, alignItems: "stretch", justifyContent: "center" }}>
-      {answersArray.map((tag, index) => {
-        console.log(index, tag);
-        return (
-          <Button
-            // style={styles.tags}
-            style={{
-              backgroundColor: "gray",
-              margin: 8,
-              borderColor: "white",
-              //borderWidth: 1,
-              borderRadius: 8,
-              color: "white",
-              width: "100%",
-              // fontSize: 24,
-              // fontWeight: 'bold',
-              overflow: "hidden",
-              padding: 12,
-              textAlign: "center"
-            }}
-            key={index}
-            title={tag}
-          />
-        );
+    <View>
+    <View style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          flex: 1,
+          justifyContent: "space-around",
+          alignItems: "flex-end",
+          width: width,
+        }}>
+
+      {answersArray.map((tag, index) => {      
+          return (
+            <Button
+              // style={styles.tags}
+              style={{
+                backgroundColor: "gray",
+                //marginTop: 8,
+                borderColor: "white",
+                //borderWidth: 1,
+                borderRadius: 8,
+                color: "white",
+                width: width/6,
+                // fontSize: 24,
+                // fontWeight: 'bold',
+                overflow: "hidden",
+                padding: 12,
+                textAlign: "center"
+              }}
+              key={index}
+              title={tag}
+            />
+          );      
       })}
+     </View>
       {/* button to add more tags */}
       {addLoopButton ? 
+       
         <GoToButton 
           style={{ alignItems: "center" }} 
           screenName="Input" 
@@ -46,7 +60,9 @@ function TagsComponent(props) {
             question, questionNumber, allQuestionsSum, iconName:"search",
             position: "center"
           }} 
-        /> : null}
+        />
+         : null}
+        
     </View>
   );
 }
