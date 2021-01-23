def5e5c117c3b521db7cdd2d5a1094ff26bc6694
RadekKosiada <kosiadar@gmail.com>
2021-01-24 00:02:44 +0100

    Iterate through categories settings; Add  components for each setting

--- components/main-screens/settings.js
+++ components/main-screens/settings.js
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, CommonActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
@@ -15,24 +15,90 @@ import {
   Dimensions
 } from "react-native";
 // import { Divider } from 'react-native-elements';
-import { AntDesign } from '@expo/vector-icons'; 
+import { AntDesign } from "@expo/vector-icons";
 
-const SettingsButton = ({ navigation, route }) => {
-  console.log("SETTINGS ROUTE", route);
+const questionsData = require("./questions.json");
+const settingCategoriesArr = questionsData.settingsCategories;
+
+// const SettingsButton = props => {
+//   return (
+//     <TouchableOpacity style={styles.button}>
+//       <Text
+//         style={styles.buttonText}
+//       >
+//         {props.text}
+//       </Text>
+//       <AntDesign
+//         size={20}
+//         color="black"
+//         style={styles.buttonIcon}
+//         name="right"
+//       />
+//     </TouchableOpacity>
+//   );
+// };
+
+function ContactUs() {
   return (
-    <TouchableOpacity style={styles.button}>
-      <Text style={styles.buttonText }>Placeholder</Text> 
-      <AntDesign size={20} color="black" style={styles.buttonIcon} name="right" />
+    <View>
+      <Text>We are a small, non profit team working... </Text>
+      <Text>Do you have any questions?</Text>
+      <Text>Subject: </Text>
+      <TextInput></TextInput>
+      <Text>Message: </Text>
+      <TextInput></TextInput>
+    </View>
+  );
+}
+
+function EditQuestions() {
+  return (
+    <TouchableOpacity>
+      <Text>Questions</Text>
     </TouchableOpacity>
   );
-};
+}
+
+function CalendarPreferences() {
+  return (
+      <Text>Week starts on:</Text>
+  )
+}
+
+function DeleteData () {
+  return (<Text>Delete all my data..... </Text>)
+}
+
+//like QuestionScreen
+function SettingScreen({ route, navigation }) {
+  console.log("SettingScreen: ", route.name);
+  if (route.name === "Contact us") {
+    return <ContactUs />;
+  } else if (route.name === "Edit questions") {
+    return <EditQuestions />
+  } else if (route.name === "Calendar preferences") {
+    return <CalendarPreferences />
+  } else if (route.name === "Delete data") {
+    return <DeleteData />
+  }
+}
+// Root
+const SettingsStack = createStackNavigator();
 
 function SettingsStackScreen() {
- 
   return (
     <SettingsStack.Navigator>
       <SettingsStack.Screen name="Settings" component={Settings} />
 
+      {settingCategoriesArr.map((category, index) => {
+        return (
+          <SettingsStack.Screen
+            name={category}
+            key={index}
+            component={SettingScreen}
+          />
+        );
+      })}
     </SettingsStack.Navigator>
   );
 }
@@ -40,37 +106,43 @@ function SettingsStackScreen() {
 const Settings = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
-      <Text style={styles.header}>Settings</Text>
+      {/* <Text style={styles.header}>Settings</Text> */}
       <View style={styles.subView}>
-        <SettingsButton
-        
-        />
-        <SettingsButton />
-        <SettingsButton />
-        <SettingsButton />
-
-        <TouchableOpacity>
-          <Text style={styles.text}>Edit questions</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text style={styles.text}>Calendar preferences</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text style={styles.text}>Delete data</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text style={styles.text}>Contact us</Text>
-        </TouchableOpacity>
+        {settingCategoriesArr.map((category, index) => {
+          return (
+            <TouchableOpacity key={index} style={styles.button}>
+              <Text
+                style={styles.buttonText}
+                onPress={() =>
+                  navigation.dispatch(
+                    CommonActions.navigate({
+                      name: category,
+                      params: {}
+                    })
+                  )
+                }
+              >
+                {category}
+              </Text>
+              <AntDesign
+                size={20}
+                color="black"
+                style={styles.buttonIcon}
+                name="right"
+              />
+            </TouchableOpacity>
+          );
+        })}
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {  flexDirection: "row"},
+  button: {
+    flexDirection: "row",
+    width: Dimensions.get("window").width
+  },
   header: {
     color: "black",
     fontSize: 22,
@@ -89,18 +161,17 @@ const styles = StyleSheet.create({
     fontSize: 22
   },
   buttonText: {
-    alignSelf: 'flex-start',
-      marginLeft: 30,
-      marginTop: 20,
-      color: "black",
-      fontSize: 22
-
+    alignSelf: "flex-start",
+    marginLeft: 30,
+    marginTop: 20,
+    color: "black",
+    fontSize: 22
   },
   buttonIcon: {
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     //position of this arrow needs adjustment
-    marginLeft: 200
+    margin: 3
   }
 });
 
-export default Settings;
+export default SettingsStackScreen;
