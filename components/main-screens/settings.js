7d4271684ef546e28959cea8112d38373e4aec69
RadekKosiada <kosiadar@gmail.com>
2021-01-23 21:14:45 +0100

    Adjust Settings screen

--- components/main-screens/settings.js
+++ components/main-screens/settings.js
@@ -10,17 +10,54 @@ import {
   View,
   TextInput,
   Image,
-  Button
+  Button,
+  TouchableOpacity,
+  Dimensions
 } from "react-native";
 
 const Settings = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings</Text>
-      
-     
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <Text style={styles.header}>Settings</Text>
+      <View style={styles.subView}>
+        <TouchableOpacity>
+          <Text style={styles.text}>Edit questions</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.text}>Calendar preferences</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.text}>Delete data</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.text}>Contact us</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    color: "black",
+    fontSize: 22,
+    marginTop: 30
+  },
+  subView: {
+    marginTop: 60,
+    flex: 1,
+    justifyContent: "flex-start",
+    alignSelf: "flex-start"
+  },
+  text: {
+    marginLeft: 30,
+    marginTop: 20,
+    color: "black",
+    fontSize: 22
+  }
+});
 
 export default Settings;
