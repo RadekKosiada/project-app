e92c6f7a01c89b59d87379d6e108c5423ea877c6
RadekKosiada <kosiadar@gmail.com>
2021-02-27 20:51:09 +0100

    Create addQuestion component

---
+++ components/main-screens/addQuestion.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  TextInput
+} from "react-native";
+
+function AddQuestion() {
+  return (
+    <View>
+      <Text style={{ fontSize: 22, color: "black" }}>Delete all my data: </Text>
+      <Text>Question</Text>
+      <TextInput></TextInput>
+      
+       
+
+     
+       
+      
+    </View>
+  );
+}
+
+export default AddQuestion;
