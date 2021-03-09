c9d00bdb474e7815f262c0e33e2d0eaad44e42c4
RadekKosiada <kosiadar@gmail.com>
2021-03-09 20:44:47 +0100

    Add actions to reducer

--- components/main-screens/reducers/questions.js
+++ components/main-screens/reducers/questions.js
@@ -1,3 +1,27 @@
-const questions = (state=[], action) => {
-  return state
-}
+const questions = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_QUESTION":
+      return [
+        ...state,
+        {
+          //id: action.id,
+          id: 99,
+          question: action.question,
+          possibleAnswer: "scale",
+          visible: true
+        }
+      ];
+    case "TOGGLE_QUESTION":
+      return state.map(question =>
+        question.id === action.id
+          ? { ...question, visible: !question.visible }
+          : question
+      );
+
+    default:
+      return state;
+  }
+  return state;
+};
+
+export default questions;
